2220057ad14ddc66cecd9da71488d07ea36257f9
Gustavo Diniz <131290288+Gugzz21@users.noreply.github.com>
2025-09-16 10:42:25 -0300

    Add files via upload

---
+++ components/Timer/index.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text } from "react-native";
+
+
+export const Timer = () => {
+    <Text style={styles.timer}> 
+        {new Date(timerType.initialValue * 1000).toLocaleTimeString("pt-BR", { minute: "2-digit", second: "2-digit" })} 
+    </Text>
+}
+
+const styles = StyleSheet.create({
+    timer: {
+      fontSize: 54,
+      color: '#FFF',
+      fontWeight: "bold",
+      textAlign: "center",
+    }
+  })
+  
+  
